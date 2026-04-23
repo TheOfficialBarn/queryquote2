8cfb688e8b9446624f3b97bd3da583dabf301b8d
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2026-04-23 14:41:08 -0500

    first commit

---
+++ app/layout.js
@@ -0,0 +1,42 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "QueryQuote",
+  description: "QueryQuote app",
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+    shortcut: ["/favicon.ico"],
+  },
+};
+
+export const viewport = {
+  themeColor: "#000000",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-screen flex flex-col bg-[url('/background.jpg')]! bg-cover bg-center bg-no-repeat">{children}</body>
+    </html>
+  );
+}
